702c2a26feadc16ac038c591539489fe29342b7e
kvalerio19 <kvalerio19@hotmail.es>
2021-11-25 20:49:01 -0600

    Changes

--- SumGame/components/Game.js
+++ SumGame/components/Game.js
@@ -4,11 +4,23 @@ import React from 'react';
 
 const target = 10 + Math.floor(40* Math.random());
 
-export default Game = ({randomNumbersCount})=>(
+
+
+export default Game = ({randomNumbersCount})=>{
+ const randomNumbers = Array.from({length: randomNumbersCount})
+ .map(()=> 1 + Math.floor(10 * Math.random()));
+ const target = randomNumbers.slice(0, randomNumbersCount - 2)
+ .reduce((acc, cur)=> acc+cur,0);
+
+    return(
     <View>
         <Text style={styles.target}>{target}</Text>
+        {randomNumbers.map((randomNumber, index)=> (
+        <Text key={index}>{randomNumber}</Text>
+        ))}
     </View>
-);
+    );
+    };
 
 
 const styles = StyleSheet.create({
